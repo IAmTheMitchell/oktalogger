bc52f9194ce2915cf33a4be09e5447507e22e35c
Mitchell Carlson <mitchell.carlson.pro@gmail.com>
2025-05-13 16:56:33 -0500

    feat: retrieve logs from Okta

---
+++ README.md
@@ -0,0 +1,6 @@
+# Coding Challenge 2
+
+The goal of this project is to poll Okta for audit logs and send them to a SIEM. The project contains Python code for a Lambda function to facilitate forwarding the logs and Terraform code to automate the infrastructure setup. 
+
+## Set Up
+1. Follow [the Okta documentation](https://developer.okta.com/docs/guides/create-an-api-token/main/) to create an Okta API token.
--- main.py
+++ main.py
@@ -1,5 +1,20 @@
+import os
+
+import requests
+
+
 def main():
-    print("Hello from rchallenge2!")
+    baseurl = "https://dev-50024099-admin.okta.com"
+    endpoint = baseurl + "/api/v1/logs"
+
+    api_key = os.environ.get("OKTA_API_KEY")
+
+    headers = {"Authorization": f"SSWS {api_key}"}
+
+    response = requests.get(endpoint, headers=headers)
+
+    data = response.json()
+    print(data)
 
 
 if __name__ == "__main__":
